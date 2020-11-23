ddf310a072f1e48d6388b8a0dbc3cb85e1178c14
Timothy Liu <tlkh.xms@gmail.com>
2020-11-24 01:20:16 +0800

    update readme and app

--- README.md
+++ README.md
@@ -1,19 +1,39 @@
-# Transformers Benchmarking
+# 🤗 Transformers Benchmarking
 
-just for fun only
+Benchmark the performance of 🤗 Transformers on PyTorch. Quick project done out of curiosity. 
 
-## Setup
+Live link: https://share.streamlit.io/tlkh/transformers-benchmarking/main/app.py
 
 ```shell
 docker pull nvcr.io/nvidia/pytorch:20.10-py3
 
 # inside container
 pip install -r requirements.txt -U
+
+# edit EXP_PREFIX to change directory to record results
+nano run_benchmarks_1gpu.sh
+
+# run benchmarking script
+run_benchmarks_1gpu.sh
 ```
 
-## Running
+### More Information
 
-```shell
-# single GPU test
-bash run_benchmarks_1gpu.sh
-```
+**Important disclaimer**\n
+Benchmarks are done by myself independently on with NVIDIA NGC PyTorch 20.10 container. The information is provided on a best-effort basis and I make no guarantees of the accuracy of the information. Feel free to [open a GitHub issue](https://github.com/tlkh/transformers-benchmarking/issues/new) if you have any questions or suggestions.
+
+This dashboard shows the measured performance of GPUs when training various configurations of Transformer networks, showing throughput (seq/s) and GPU memory (VRAM) usage. The idea is to allow users have an easy reference for choosing model configuration (model size/batch size/sequence length) and GPU model.
+
+Each HuggingFace Transformer model is loaded in PyTorch as a `AutoModelForSequenceClassification` and used in a PyTorch Lightning `LightningModule` for training.
+
+Forward and backward pass computation timings and provided by the PyTorch Lightning built-in [`SimpleProfiler`](https://pytorch-lightning.readthedocs.io/en/latest/profiler.html#enable-simple-profiling).
+
+GPU utilization shown is likely lower than actual since measuring GPU utilization (especially using [DLProf](https://docs.nvidia.com/deeplearning/frameworks/dlprof-user-guide/) to measure Tensor Core utilization) likely reduces GPU utilization due to overheads. This is not present in the throughput measurements.
+
+All current results are using PyTorch [native automatic mixed precision](https://pytorch.org/blog/accelerating-training-on-nvidia-gpus-with-pytorch-automatic-mixed-precision/) and [Apex fused AdamW optimizer](https://nvidia.github.io/apex/optimizers.html).
+
+Systems used:
+* V100 - local OEM system
+* A100 - AWS p4d.24xlarge instance
+
+Only single GPU configurations have been tested.
--- app.py
+++ app.py
@@ -117,16 +117,28 @@ with col2:
 
 with st.beta_expander(label="More information", expanded=True):
     st.markdown("""**Important disclaimer**\n
-Benchmarks are done by myself independently on with NVIDIA NGC PyTorch 20.10 container. The information is provided on a best-effort basis and I make no guarantees of the accuracy of the information. Feel free to [open a GitHub issue](https://github.com/tlkh/transformers-benchmarking/issues/new) if you have any questions or suggestions.""")
-    st.markdown("This dashboard shows the measured performance of GPUs when training various configurations of Transformer networks, showing throughput (seq/s) and GPU memory (VRAM) usage. The idea is to allow users have an easy reference for choosing model configuration (model size/batch size/sequence length) and GPU model.")
-    st.markdown("Each HuggingFace Transformer model is loaded in PyTorch as a `AutoModelForSequenceClassification` and used in a PyTorch Lightning `LightningModule` for training.")
-    st.markdown("Forward and backward pass computation timings and provided by the PyTorch Lightning built-in [`SimpleProfiler`](https://pytorch-lightning.readthedocs.io/en/latest/profiler.html#enable-simple-profiling).")
-    st.markdown("GPU utilization shown is likely lower than actual since measuring GPU utilization (especially using [DLProf](https://docs.nvidia.com/deeplearning/frameworks/dlprof-user-guide/) to measure Tensor Core utilization) likely reduces GPU utilization due to overheads. This is not present in the throughput measurements.")
-    st.markdown("""Systems used:
+Benchmarks are done by myself independently on with NVIDIA NGC PyTorch 20.10 container. The information is provided on a best-effort basis and I make no guarantees of the accuracy of the information. Feel free to [open a GitHub issue](https://github.com/tlkh/transformers-benchmarking/issues/new) if you have any questions or suggestions.
+
+This dashboard shows the measured performance of GPUs when training various configurations of Transformer networks, showing throughput (seq/s) and GPU memory (VRAM) usage. The idea is to allow users have an easy reference for choosing model configuration (model size/batch size/sequence length) and GPU model.
+
+Each HuggingFace Transformer model is loaded in PyTorch as a `AutoModelForSequenceClassification` and used in a PyTorch Lightning `LightningModule` for training.
+
+Forward and backward pass computation timings and provided by the PyTorch Lightning built-in [`SimpleProfiler`](https://pytorch-lightning.readthedocs.io/en/latest/profiler.html#enable-simple-profiling).
+
+GPU utilization shown is likely lower than actual since measuring GPU utilization (especially using [DLProf](https://docs.nvidia.com/deeplearning/frameworks/dlprof-user-guide/) to measure Tensor Core utilization) likely reduces GPU utilization due to overheads. This is not present in the throughput measurements.
+
+All current results are using PyTorch [native automatic mixed precision](https://pytorch.org/blog/accelerating-training-on-nvidia-gpus-with-pytorch-automatic-mixed-precision/) and [Apex fused AdamW optimizer](https://nvidia.github.io/apex/optimizers.html).
+
+Systems used:
 * V100 - local OEM system
-* A100 - AWS p4d.24xlarge instance""")
-    st.markdown("Only single GPU configuration has been tested.")
-    st.markdown("All current results are using PyTorch [native automatic mixed precision](https://pytorch.org/blog/accelerating-training-on-nvidia-gpus-with-pytorch-automatic-mixed-precision/) and [Apex fused AdamW optimizer](https://nvidia.github.io/apex/optimizers.html).")
+* A100 - AWS p4d.24xlarge instance
+
+Only single GPU configurations have been tested.
+""")
+    st.markdown("""GPU utilization values:
+* sm_util: % of time one or more kernels was executing on the GPU
+* tc_util: % of compute time using Tensor Core
+* vram_io: % of time GPU memory was being read or written""")
     st.markdown("GitHub repository: https://github.com/tlkh/transformers-benchmarking")
 
 with st.beta_expander(label="Raw Data Table", expanded=False):
